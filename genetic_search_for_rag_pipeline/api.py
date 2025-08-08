9b397020557ff5c40c81027bea409b8ad62b76b3
yAquila <yusufkartal2004@gmail.com>
2025-08-08 08:53:40 +0300

    minor change

--- genetic_search_for_rag_pipeline/api.py
+++ genetic_search_for_rag_pipeline/api.py
@@ -579,7 +579,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "api:app",
         host="0.0.0.0",
-        port=8000,
+        port=8009,
         reload=False,
         log_level="info"
     ) 
